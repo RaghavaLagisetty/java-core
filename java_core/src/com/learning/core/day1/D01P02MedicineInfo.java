3eee06ac075256d388f0867cd0a55fdfae3eab1e
RaghavaLagisetty <145760733+RaghavaLagisetty@users.noreply.github.com>
2024-03-05 00:03:34 +0530

    Delete java_core/src/com/learning/core/day1 directory

--- java_core/src/com/learning/core/day1/D01P02MedicineInfo.java
+++
@@ -1,5 +0,0 @@
-package com.learning.core.day1;
-
-public interface D01P02MedicineInfo {
-	void displayLabel();
-}
--- java_core/src/com/learning/core/day1/D01P02Syrup.java
+++
@@ -1,8 +0,0 @@
-package com.learning.core.day1;
-
-public class D01P02Syrup implements D01P02MedicineInfo {
-	@Override
-	public void displayLabel() {
-		System.out.println("Syrup is consumble only on prescription");
-	}
-}
