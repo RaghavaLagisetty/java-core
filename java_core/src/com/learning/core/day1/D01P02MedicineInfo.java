b6315a948e6a4fb77653e6a15e468354bea6ed34
RaghavaLagisetty <145760733+RaghavaLagisetty@users.noreply.github.com>
2024-03-04 16:07:48 +0530

    Add files via upload

---
+++ java_core/src/com/learning/core/day1/D01P02MedicineInfo.java
@@ -0,0 +1,5 @@
+package com.learning.core.day1;
+
+public interface D01P02MedicineInfo {
+	void displayLabel();
+}
---
+++ java_core/src/com/learning/core/day1/D01P02Syrup.java
@@ -0,0 +1,8 @@
+package com.learning.core.day1;
+
+public class D01P02Syrup implements D01P02MedicineInfo {
+	@Override
+	public void displayLabel() {
+		System.out.println("Syrup is consumble only on prescription");
+	}
+}
